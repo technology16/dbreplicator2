360686fcbbee482fa212d6d6d65331cb7440cb1b
Andrew G. Saushkin <ags@bsse.ru>
2013-10-23 09:19:43 +0400

    Исправлено инициализация пула соединений с БД, в случае его отсутствия

--- src/main/java/ru/taximaxim/dbreplicator2/cf/BoneCPConnectionsFactory.java
+++ src/main/java/ru/taximaxim/dbreplicator2/cf/BoneCPConnectionsFactory.java
@@ -28,6 +28,8 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import com.jolbox.bonecp.BoneCP;
 import com.jolbox.bonecp.BoneCPConfig;
 
@@ -38,6 +40,8 @@ import com.jolbox.bonecp.BoneCPConfig;
  *
  */
 public class BoneCPConnectionsFactory implements ConnectionsFactory {
+	
+	private static final Logger LOG = Logger.getLogger(BoneCPConnectionsFactory.class);
 
     /**
      * Инициализированные именнованные пулы соединений 
@@ -70,6 +74,11 @@ public class BoneCPConnectionsFactory implements ConnectionsFactory {
             if (connectionPool == null) {
                 BoneCPSettings boneCPSettings = settingStorage.getDataBaseSettingsByName(poolName);
 
+                if (boneCPSettings == null) {
+                	LOG.error("Не найден пул соединений с базой данных: " + poolName);
+                	return null;
+                }
+                
                 Class.forName(boneCPSettings.getDriver());
 
                 BoneCPConfig config = new BoneCPConfig();
