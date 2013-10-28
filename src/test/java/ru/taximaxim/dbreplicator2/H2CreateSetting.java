22543e77a2bba533d7c4b90c91159860511461a6
NeitrinoZull <mardanov_rm@taximaxim.ru>
2013-10-28 15:49:30 +0400

    jUnit

--- src/test/java/ru/taximaxim/dbreplicator2/H2CreateSetting.java
+++ src/test/java/ru/taximaxim/dbreplicator2/H2CreateSetting.java
@@ -23,9 +23,9 @@
 
 package ru.taximaxim.dbreplicator2;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
@@ -60,19 +60,19 @@ public class H2CreateSetting {
     public static void setUpAfterClass() throws Exception {
         
     }
-
-    
     
     @Test
-    public void testConnection() throws SQLException, ClassNotFoundException {
+    public void testConnection() throws SQLException, ClassNotFoundException, IOException {
 
         LOG.debug("Start: ");
         String source = "source";
         Connection conn = connectionFactory.getConnection(source);
         
-        Helper.CreateTebleRep2(conn);
-        CreateTeble(conn);
-
+        Helper.executeSqlFromFile(conn, "importRep2.sql");
+        Helper.executeSqlFromFile(conn, "importSource.sql");
+        createTrigger(conn);
+        Helper.executeSqlFromFile(conn, "importSourceData.sql");
+        
         LOG.info("<======Inception======>");
         Helper.InfoSuperLog(conn);
         LOG.info(">======Inception======<");
@@ -91,94 +91,20 @@ public class H2CreateSetting {
         conn.close();
     }
     
-    public void CreateTeble(Connection conn)
+    /**
+     * Создание триггера
+     */
+    public void createTrigger(Connection conn)
             throws SQLException, ClassNotFoundException {
-
-        Statement stat = conn.createStatement();
-
-        Helper.delete(stat, "T_TABLE1");
-        Helper.delete(stat, "T_TABLE2");
-        Helper.delete(stat, "T_TABLE3");
-        Helper.delete(stat, "T_TABLE4");
-        Helper.delete(stat, "T_TABLE5");
-        Helper.delete(stat, "T_TABLE6");
-        Helper.delete(stat, "T_TABLE7");
-        Helper.delete(stat, "T_TABLE8");
-        Helper.delete(stat, "T_TABLE9");
-        Helper.delete(stat, "T_TABLE0");
-        
-        stat.execute("CREATE TABLE T_TABLE1(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE1");
-
-        stat.execute("CREATE TABLE T_TABLE2(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE2");
-
-        stat.execute("CREATE TABLE T_TABLE3(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE3");
-
-        stat.execute("CREATE TABLE T_TABLE4(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE4");
-
-        stat.execute("CREATE TABLE T_TABLE5(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE5");
-
-        stat.execute("CREATE TABLE T_TABLE6(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE6");
-
-        stat.execute("CREATE TABLE T_TABLE7(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE7");
-
-        stat.execute("CREATE TABLE T_TABLE8(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE8");
-
-        stat.execute("CREATE TABLE T_TABLE9(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE9");
-
-        stat.execute("CREATE TABLE T_TABLE0(ID INT PRIMARY KEY, C_AMOUNT DECIMAL, C_NAME VARCHAR(50))");
         Helper.createTrigger(conn, "T_TABLE0");
-        
-        InsertInto(conn);
-    }
-
-    public void InsertInto(Connection conn)
-            throws SQLException, ClassNotFoundException {
-
-        Statement stat = conn.createStatement();
-        try {
-
-            stat.execute("INSERT INTO T_TABLE1 VALUES(1, 10.00, 'TESTER')");
-            stat.execute("INSERT INTO T_TABLE1 VALUES(2, 19.95, 'TESTER')");
-            stat.execute("UPDATE T_TABLE1 SET C_AMOUNT = 20.0 WHERE ID = 2");
-            stat.execute("DELETE FROM T_TABLE1 WHERE ID = 1");
-
-            stat.execute("INSERT INTO T_TABLE2 VALUES(1, 14.50, 'TESTER')");
-            stat.execute("INSERT INTO T_TABLE2 VALUES(2, 12.55, 'TESTER')");
-            stat.execute("UPDATE T_TABLE2 SET C_AMOUNT = 60.0 WHERE ID = 2");
-            stat.execute("DELETE FROM T_TABLE2 WHERE ID = 1");
-
-            stat.execute("INSERT INTO T_TABLE3 VALUES(1, 05.00, 'TESTER')");
-            stat.execute("INSERT INTO T_TABLE3 VALUES(2, 78.55, 'TESTER')");
-            stat.execute("UPDATE T_TABLE3 SET C_AMOUNT = 67.99 WHERE ID = 2");
-            stat.execute("DELETE FROM T_TABLE3 WHERE ID = 1");
-
-            stat.execute("INSERT INTO T_TABLE4 VALUES(1, 37.00, 'TESTER')");
-            stat.execute("INSERT INTO T_TABLE4 VALUES(2, 13.88, 'TESTER')");
-            stat.execute("UPDATE T_TABLE4 SET C_AMOUNT = 23.78 WHERE ID = 2");
-            stat.execute("DELETE FROM T_TABLE4 WHERE ID = 1");
-
-            stat.execute("INSERT INTO T_TABLE5 VALUES(1, 86.00, 'TESTER')");
-            stat.execute("INSERT INTO T_TABLE5 VALUES(2, 99.99, 'TESTER')");
-            stat.execute("UPDATE T_TABLE5 SET C_AMOUNT = 10.09 WHERE ID = 2");
-            stat.execute("DELETE FROM T_TABLE5 WHERE ID = 1");
-
-            stat.execute("INSERT INTO T_TABLE6 VALUES(1, 44.55, 'TESTER')");
-            stat.execute("INSERT INTO T_TABLE6 VALUES(2, 36.47, 'TESTER')");
-            stat.execute("UPDATE T_TABLE6 SET C_AMOUNT = 79.80 WHERE ID = 2");
-            stat.execute("DELETE FROM T_TABLE6 WHERE ID = 1");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
-        }
     }
 }
--- src/test/java/ru/taximaxim/dbreplicator2/Helper.java
+++ src/test/java/ru/taximaxim/dbreplicator2/Helper.java
@@ -1,5 +1,8 @@
 package ru.taximaxim.dbreplicator2;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,65 +12,36 @@ import java.sql.Statement;
 
 import org.apache.log4j.Logger;
 import org.h2.api.Trigger;
-import org.hibernate.Session;
-
-import ru.taximaxim.dbreplicator2.model.BoneCPSettingsModel;
-import ru.taximaxim.dbreplicator2.model.RunnerModel;
-import ru.taximaxim.dbreplicator2.model.StrategyModel;
 
 public class Helper {
     
     protected static final Logger LOG = Logger.getLogger(Helper.class);
-    
-    public static void setupConn(Session session, String name)
-            throws ClassNotFoundException, SQLException {
-        BoneCPSettingsModel settings = new BoneCPSettingsModel(name,"org.h2.Driver", "jdbc:h2:mem://localhost/~/" + name, "sa", "");
-        session.beginTransaction();
-        session.saveOrUpdate(settings);
-        session.getTransaction().commit();
-    }
 
-    public static void CreateTebleRep2(Connection conn) throws SQLException, ClassNotFoundException {
+    /**
+     * Выполнение скрипта из текстового файла
+     * 
+     * @param connection - соединение с целевой БД
+     * @param fileName - полное имя файла
+     * 
+     * @throws IOException
+     * @throws SQLException
+     */
+    protected static void executeSqlFromFile(Connection connection, String fileName) throws IOException, SQLException{
+        String curDir = new File("").getAbsolutePath() + "/src/test/resources/" + fileName;
+        FileInputStream sqlFile = new FileInputStream(curDir);
+        byte[] sqlBytes = new byte[sqlFile.available()];
+        sqlFile.read(sqlBytes);
+        sqlFile.close();
 
-        try {
-            Statement stat = conn.createStatement();
+        String sqlText = new String(sqlBytes);
 
-            delete(stat, "rep2_superlog");
-            delete(stat, "rep2_workpool_data");
-
-            stat.execute("CREATE TABLE rep2_superlog(id_superlog IDENTITY PRIMARY KEY, id_foreign INTEGER, id_table NVARCHAR, c_operation VARCHAR(1), c_date TIMESTAMP, id_transaction NVARCHAR);");
-            stat.execute("CREATE TABLE rep2_workpool_data(id_runner INTEGER, id_superlog BIGINT, id_foreign INTEGER, id_table NVARCHAR, c_operation VARCHAR(1), c_date TIMESTAMP, id_transaction NVARCHAR);");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
-        }
-    }
-
-    public static void delete(Statement stat, String tableName) throws SQLException {
-        try {
-            stat.execute("Drop Table if exists " + tableName);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
-        }
+        PreparedStatement statement = connection.prepareStatement(sqlText);
+        statement.execute();
     }
     
-    public static void InfoTest(Connection conn) throws SQLException{
-        Statement stat = conn.createStatement();
-        ResultSet rsS = stat.executeQuery("select * from runners");
-        ResultSetMetaData rDataS = rsS.getMetaData();
-        int totalColumnS = rDataS.getColumnCount();
-        while (rsS.next()) {
-            String text = "";
-            for (int i = 1; i <= totalColumnS; i++) {
-                text += rsS.getObject(i).toString() + "\t";
-            }
-            LOG.info(text);
-            LOG.info("================================================================");
-        }
-    }
-    
+    /**
+     * Вывод таблицы rep2_superlog
+     */
     public static void InfoSuperLog(Connection conn) throws SQLException{
         Statement stat = conn.createStatement();
         ResultSet rsS = stat.executeQuery("select id_superlog, id_foreign, id_table, c_operation, c_date, id_transaction from rep2_superlog");
@@ -83,6 +57,9 @@ public class Helper {
         }
     }
     
+    /**
+     * Вывод таблицы rep2_workpool_data
+     */
     public static void InfoWorkPoolData(Connection conn) throws SQLException{
         
         Statement stat = conn.createStatement();
@@ -96,10 +73,13 @@ public class Helper {
                 text += rsQ.getObject(i).toString() + "\t";
             }
             LOG.info(text);
-            LOG.info("=====================");
+            LOG.info("========================================================================");
         }
     }
 
+    /**
+     * Создание Триггера
+     */
     public static void createTrigger(Connection conn, String tableName) throws SQLException {
 
         Statement stat = conn.createStatement();
@@ -160,59 +140,5 @@ public class Helper {
         public void remove() throws SQLException {
 
         }
-    }
-
-    /**
-     * Создание RunnerModel
-     * @return RunnerModel
-     */
-    public static RunnerModel createRunner(String className, String source, String target,
-            String description) {
-
-        RunnerModel runner = new RunnerModel();
-        runner.setClassName(className);
-        runner.setSource(source);
-        runner.setTarget(target);
-        runner.setDescription(description);
-
-        return runner;
-    }
-
-    /**
-     * Создание старатегии
-     * @return StrategyModel
-     */
-    public static StrategyModel createStrategy(String className, String param,
-            boolean isEnabled, int priority) {
-
-        StrategyModel strategy = new StrategyModel();
-
-        strategy.setClassName(className);
-        strategy.setParam(param);
-        strategy.setEnabled(isEnabled);
-        strategy.setPriority(priority);
-
-        return strategy;
-    }
-    
-
-    /**
-     * Создание RunnerModel и Запись в базу 
-     */
-    public static void createRunnerModel(Session session, String source, String target) {
-
-        RunnerModel model = 
-                createRunner(RunnerModel.REPLICA_RUNNER_CLASS, source, target, "description");
-        StrategyModel strategyModel = createStrategy(
-                "ru.taximaxim.dbreplicator2.replica.strategies.DummyStrategy",
-                null, true, 100);
-
-        model.getStrategyModels().add(strategyModel);
-        strategyModel.setRunner(model);
-
-        session.beginTransaction();
-        session.saveOrUpdate(model);
-        session.getTransaction().commit();
-        
     }
 }
