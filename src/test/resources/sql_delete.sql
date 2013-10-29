547733e503d9a36816e73b9bc724bc9fda40cc2a
NeitrinoZull <mardanov_rm@taximaxim.ru>
2013-10-29 17:14:15 +0400

    j unit foreign key

---
+++ src/test/resources/sql_delete.sql
@@ -0,0 +1,3 @@
+DELETE FROM t_table WHERE _int = 1;
+DELETE FROM t_table1 WHERE _int = 2;
+DELETE FROM t_table WHERE _int = 3;
---
+++ src/test/resources/sql_update.sql
@@ -0,0 +1,6 @@
+UPDATE t_table SET _boolean = true, _long = 0, _decimal = 0, _double = 0, _float = 0, _string = 'Update', _byte = 0, _date = now(), _time = now(), _timestamp = now() WHERE _int = 1;
+UPDATE t_table SET _boolean = true, _long = 0, _decimal = 0, _double = 0, _float = 0, _string = 'Update', _byte = 0, _date = now(), _time = now(), _timestamp = now() WHERE _int = 2;
+UPDATE t_table SET _boolean = true, _long = 0, _decimal = 0, _double = 0, _float = 0, _string = 'Update', _byte = 0, _date = now(), _time = now(), _timestamp = now() WHERE _int = 3;
+UPDATE t_table SET _boolean = true, _long = 0, _decimal = 0, _double = 0, _float = 0, _string = 'Update', _byte = 0, _date = now(), _time = now(), _timestamp = now() WHERE _int = 4;
+UPDATE t_table SET _boolean = true, _long = 0, _decimal = 0, _double = 0, _float = 0, _string = 'Update', _byte = 0, _date = now(), _time = now(), _timestamp = now() WHERE _int = 5;
+UPDATE t_table SET _boolean = true, _long = 0, _decimal = 0, _double = 0, _float = 0, _string = 'Update', _byte = 0, _date = now(), _time = now(), _timestamp = now() WHERE _int = 6;
