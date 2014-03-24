bae09f5245dc224209613e0cb25b8e42de00d621
NeitrinoZull <mardanov_rm@taximaxim.ru>
2014-03-24 11:51:42 +0400

    Доработать стратегии rep2_superlog менеджеров, сделать запуск раннеров после первого батча данных в rep2_workpool_data

---
+++ src/main/java/ru/taximaxim/dbreplicator2/replica/strategies/superlog/GeneiricManager.java
@@ -0,0 +1,176 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Technologiya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package ru.taximaxim.dbreplicator2.replica.strategies.superlog;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import ru.taximaxim.dbreplicator2.jdbc.Jdbc;
+import ru.taximaxim.dbreplicator2.model.BoneCPSettingsModel;
+import ru.taximaxim.dbreplicator2.model.RunnerModel;
+import ru.taximaxim.dbreplicator2.model.StrategyModel;
+import ru.taximaxim.dbreplicator2.model.TableModel;
+import ru.taximaxim.dbreplicator2.replica.Strategy;
+import ru.taximaxim.dbreplicator2.replica.StrategyException;
+import ru.taximaxim.dbreplicator2.replica.strategies.replication.StrategySkeleton;
+import ru.taximaxim.dbreplicator2.replica.strategies.replication.workpool.WorkPoolService;
+
+/**
+ * Класс стратегии менеджера записей суперлог таблицы
+ * 
+ * @author volodin_aa
+ * 
+ */
+public abstract class GeneiricManager extends StrategySkeleton implements Strategy {
+
+    private static final Logger LOG = Logger.getLogger(GeneiricManager.class);
+    
+    public GeneiricManager() {
+    }
+    
+    @Override
+    public void execute(Connection sourceConnection, Connection targetConnection, StrategyModel data) throws StrategyException, SQLException {
+        int fetchSize = getFetchSize(data);
+        int batchSize = getBatchSize(data);
+        Boolean lastAutoCommit = null;
+        Boolean lastTargetAutoCommit = null;
+        lastAutoCommit = sourceConnection.getAutoCommit();
+        lastTargetAutoCommit = targetConnection.getAutoCommit();
+        // Начинаем транзакцию
+        sourceConnection.setAutoCommit(false);
+        targetConnection.setAutoCommit(false);
+        BoneCPSettingsModel sourcePool = data.getRunner().getSource();
+        try {
+            sourceConnection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+            targetConnection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+            // Строим список обработчиков реплик
+
+            // Переносим данные
+            try (PreparedStatement insertRunnerData = targetConnection.prepareStatement("INSERT INTO rep2_workpool_data (id_runner, id_superlog, id_foreign, id_table, c_operation, c_date, id_transaction) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                    PreparedStatement deleteSuperLog = targetConnection.prepareStatement("DELETE FROM rep2_superlog WHERE id_superlog=?");
+                    PreparedStatement selectSuperLog = sourceConnection.prepareStatement("SELECT * FROM rep2_superlog ORDER BY id_superlog");) {
+                selectSuperLog.setFetchSize(fetchSize);
+                try (ResultSet superLogResult = selectSuperLog.executeQuery();) {
+                    List<String> cols = new ArrayList<String>();
+                    cols.add(WorkPoolService.ID_SUPERLOG);
+                    cols.add(WorkPoolService.ID_POOL);
+                    cols.add(WorkPoolService.ID_FOREIGN);
+                    cols.add(WorkPoolService.ID_TABLE);
+                    cols.add(WorkPoolService.C_OPERATION);
+                    cols.add(WorkPoolService.C_DATE);
+                    cols.add(WorkPoolService.ID_TRANSACTION);
+                    Set<RunnerModel> runners = new HashSet<RunnerModel>();
+                    for (int rowsCount = 1; superLogResult.next(); rowsCount++) {
+                        // Выводим данные из rep2_superlog_table
+                        if (LOG.isDebugEnabled()) {
+                            LOG.debug(Jdbc.resultSetToString(superLogResult, cols));
+                        }
+                        // Копируем записи
+                        // Проходим по списку слушателей текущей таблицы
+                        for (TableModel table : sourcePool.getTables()) {
+                            if (LOG.isDebugEnabled()) {
+                                LOG.debug(table.getName() + " id: " + table.getTableId());
+                            }
+                            if (table.getName().equalsIgnoreCase(superLogResult.getString(WorkPoolService.ID_TABLE))) {
+                                for (RunnerModel runner : table.getRunners()) {
+                                    if (!superLogResult.getString(WorkPoolService.ID_POOL).equals(runner.getTarget().getPoolId())) {
+                                        insertRunnerData.setInt(1, runner.getId());
+                                        insertRunnerData.setLong(2, superLogResult.getLong(WorkPoolService.ID_SUPERLOG));
+                                        insertRunnerData.setInt(3, superLogResult.getInt(WorkPoolService.ID_FOREIGN));
+                                        insertRunnerData.setString(4, superLogResult.getString(WorkPoolService.ID_TABLE));
+                                        insertRunnerData.setString(5, superLogResult.getString(WorkPoolService.C_OPERATION));
+                                        insertRunnerData.setTimestamp(6, superLogResult.getTimestamp(WorkPoolService.C_DATE));
+                                        insertRunnerData.setString(7, superLogResult.getString(WorkPoolService.ID_TRANSACTION));
+                                        insertRunnerData.addBatch();
+                                        // Выводим данные из rep2_superlog_table
+                                        if (LOG.isDebugEnabled()) {
+                                            LOG.debug("INSERT");
+                                        }
+                                        runners.add(runner);
+                                    }
+                                    // Удаляем исходную запись
+                                    deleteSuperLog.setLong(1, superLogResult.getLong(WorkPoolService.ID_SUPERLOG));
+                                    deleteSuperLog.addBatch();
+                                }
+                            }
+
+                        }
+
+                        // Периодически сбрасываем батч в БД
+                        if ((rowsCount % batchSize) == 0) {
+                            insertRunnerData.executeBatch();
+                            deleteSuperLog.executeBatch();
+                            targetConnection.commit();
+                            // запускаем обработчики реплик
+                            startRunners(runners);
+                            runners.clear();
+                            LOG.info(String.format("Обработано %s строк...", rowsCount));
+                        }
+                    }
+                    insertRunnerData.executeBatch();
+                    deleteSuperLog.executeBatch();
+                    targetConnection.commit();
+                }
+            }
+        } catch (Throwable e) {
+            // В любом случае
+            targetConnection.rollback();
+            // Пробрасываем ошибку на уровень выше
+            throw e;
+        }
+        // запускаем обработчики реплик
+        startRunners(sourcePool.getRunners());
+
+        try {
+            if (lastAutoCommit != null) {
+                sourceConnection.setAutoCommit(lastAutoCommit);
+            }
+        } catch (SQLException e) {
+            // Ошибка может возникнуть если во время операции упало
+            // соединение к БД
+            LOG.warn("Ошибка при возврате автокомита в исходное состояние.", e);
+        }
+
+        try {
+            if (lastTargetAutoCommit != null) {
+                targetConnection.setAutoCommit(lastTargetAutoCommit);
+            }
+        } catch (SQLException sqlException) {
+            // Ошибка может возникнуть если во время операции упало
+            // соединение к БД
+            LOG.warn("Ошибка при возврате автокомита в исходное состояние.", sqlException);
+        }
+    }
+
+    protected abstract void startRunners(Collection<RunnerModel> runners) throws StrategyException, SQLException;
+}
