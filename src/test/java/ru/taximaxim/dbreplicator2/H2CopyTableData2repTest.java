ee8a6712448830b2781af04f94790da15478acf7
Volodin AA <volodin_aa@taximaxim.ru>
2016-03-28 18:14:34 +0300

    Увеличил REPLICATION_DELAY

--- src/test/java/ru/taximaxim/dbreplicator2/H2CopyTableData2repTest.java
+++ src/test/java/ru/taximaxim/dbreplicator2/H2CopyTableData2repTest.java
@@ -56,7 +56,7 @@ import ru.taximaxim.dbreplicator2.tp.WorkerThread;
  */
 public class H2CopyTableData2repTest extends AbstractReplicationTest {
     // Задержка между циклами репликации
-    private static final int REPLICATION_DELAY = 200;
+    private static final int REPLICATION_DELAY = 400;
     
     protected static final Logger LOG = Logger.getLogger(H2CopyTableDataTest.class);
     
